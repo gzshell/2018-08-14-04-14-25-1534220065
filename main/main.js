5b234765fd3d78a5ed985fa58e09505b0090f02b
gzshell <sun.qinglong@qq.com>
2018-08-15 12:28:45 +0800

    任意数字

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    var arr = num.split('');
+    var led = {
+        up :   ["._.","...","._.","._.","...","._.","._.","._.","._.","._."],
+        mid :  ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],
+        down : ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]
+    }
+    var l = arr.length;
+    let u="";
+    let m="";
+    let d="";
+
+    for (var i=0; i<l; i++){
+        if (i<l-1){
+            u=u+led.up[arr[i]]+" ";
+            m=m+led.mid[arr[i]]+" ";
+            d=d+led.down[arr[i]]+" ";
+        }
+        else{
+            u=u+led.up[arr[i]]+"\n";
+            m=m+led.mid[arr[i]]+"\n";
+            d=d+led.down[arr[i]]+"\n";
+        }
+    }
+    let out=u+m+d;
+    return out;
+
 };
